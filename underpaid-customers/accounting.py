cd7c4e8fe27d5f1f0550c15be140fd6e74b00d3a
Tiela Black-Law <tiela.rose@gmail.com>
2023-02-27 20:38:28 -0800

    improve variable names per solution

--- underpaid-customers/accounting.py
+++ underpaid-customers/accounting.py
@@ -1,28 +1,32 @@
+# ***********************
+# OLD CODE
+# ***********************
+
 # melon_cost = 1.00
 
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
+# customer1_name = "Joe"
+# customer1_melons = 5
+# customer1_paid = 5.00
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
+# customer2_name = "Frank"
+# customer2_melons = 6
+# customer2_paid = 6.00
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
+# customer3_name = "Sally"
+# customer3_melons = 3
+# customer3_paid = 3.00
 
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
+# customer4_name = "Sean"
+# customer4_melons = 9
+# customer4_paid = 9.50
 
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
+# customer5_name = "David"
+# customer5_melons = 4
+# customer5_paid = 4.00
 
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
+# customer6_name = "Ashley"
+# customer6_melons = 3
+# customer6_paid = 2.00
 
 
 # customer1_expected = customer1_melons * melon_cost
@@ -61,21 +65,25 @@ customer6_paid = 2.00
 #           f"expected ${customer6_expected:.2f}"
 #           )
 
-def print_underpaid_customers(path, melon_cost=1):
-    """Given a path to the orders, produce a report of discrepencies between expected and actual payment
+# ***********************
+# REFACTORED CODE
+# ***********************
+
+def print_payment_status(payment_data_filename, melon_cost=1):
+    """Given a path to the orders, produce a report of discrepencies between expected and actual payment.
 
     Opens the customer orders file at [path], parses each line for customer name, melons ordered, and total amount paid. Compares expected payment to actual payment per customer, and prints a readable report of customers who over or underpaid"""
 
     # Create a file object from the string passed in as a path
-    the_file = open(path)
+    payment_data = open(payment_data_filename)
 
     # Iterate over each line in the file
-    for line in the_file:
+    for line in payment_data:
         line = line.rstrip()  # Remove trailing whitespace from each line
-        words = line.split('|')  # Create a list of strings
+        order = line.split('|')  # Create a list of strings
 
         # Assign a meaningful variable to each item from the words list
-        cust_num, cust_name, num_melons, amt_paid = words
+        order_id, cust_name, num_melons, amt_paid = order
 
         # Change the number of melons and amount paid into floats
         num_melons = float(num_melons)
@@ -90,7 +98,7 @@ def print_underpaid_customers(path, melon_cost=1):
                 f'{cust_name} paid ${amt_paid: .2f}, expected ${expected_amt: .2f}')
 
     # Close the file
-    the_file.close()
+    payment_data.close()
 
 
-print_underpaid_customers('customer-orders.txt')
+print_payment_status('customer-orders.txt')
